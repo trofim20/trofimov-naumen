b0c1b417293178ba6adbad4a747f50f746532eea
ntrofim20 <ntrofim35@mail.ru>
2024-10-06 18:12:35 +0300

    Выполнил 1 задание

--- src/main/java/ru/naumen/collection/task1/Task1.java
+++ src/main/java/ru/naumen/collection/task1/Task1.java
@@ -1,5 +1,8 @@
 package ru.naumen.collection.task1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Дано:
  * <pre>
@@ -46,8 +49,8 @@ public class Task1
      * <p><b>Мы не забыли определить equals и hashcode у класса {@link Ticket}</b></p>
      * <p>Достаточно их определить только для id, т.к. он уникален</p>
      */
+    private Map<Ticket, Goods> ticketMap = new HashMap<>();
     public Goods getGoods(Ticket ticket) {
-        // TODO реализовать
-        return null;
+        return ticketMap.getOrDefault(ticket, Goods.EMPTY);
     }
 }
